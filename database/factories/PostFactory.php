2634a801a3eee0b4751b15fe4ea895ef3784b187
linith2244github <linithseang2244@gmail.com>
2026-02-17 21:42:07 +0700

    add j adf

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -23,9 +24,15 @@ class PostFactory extends Factory
         $userIds  = User::pluck('id')->toArray();
         //[1,2,...] => random 1 2 ....
         $userId = collect($userIds)->random();
+
+        //Date Time
+        $currentDate = Carbon::now()->format('d-m-y');
+        
         return [
             "content" => $this->faker->text(50),
-            "user_id" => $this->faker->$userId
+            "user_id" => $userId,
+            "created_at"=> $currentDate,
+            "updated_at"=> $currentDate
         ];
     }
 }
